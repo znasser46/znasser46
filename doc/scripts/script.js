e7b298994c4f9258fbf63c34c884128d954455a8
znasser46 <zacharynasser46@gmail.com>
2025-09-03 10:49:20 -0500

    Add files via upload

--- doc/scripts/script.js
+++ doc/scripts/script.js
@@ -5,3 +5,17 @@ console.log("we workin, baby!")
 document.addEventListener("DOMContentLoaded", () => {
     console.log("we workin, baby!");
   });
+
+  document.getElementById('btnClicky').addEventListener('click', btnClicky);
+// document.addEventListener("close",  () => {
+//   console.log("we workin, baby!");
+// });
+
+
+function btnClicky () {
+  btnClicky.getElementById('click', () => { document.forEach(el => { el.style.transform = 'rotate(15deg)'; }); });
+//querySelectorAll('.yourClassName')
+console.log('after rotation');
+}
+  const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { });
+  btnClicky.addEventListener('click', () => { document.getElementById('blogEntry').textContent = "Why did the pizza maker go broke? Because he just couldn't make enough dough!"; });
